c231964e9fe37ba3073f144da52b25618ca64097
Molfarion <bogdantkach47@gmail.com>
2026-04-19 14:30:34 +0300

    Created basic ORM model and loaded dataset from Kaggle

---
+++ app/models.py
@@ -0,0 +1,43 @@
+from sqlalchemy import Column, Integer, Float, String, DateTime, Time, Enum, Boolean, ForeignKey
+from sqlalchemy.orm import relationship
+from database import Base
+import enum
+
+class WindDirection(enum.Enum):
+    N = "N"
+    S = "S"
+    E = "E"
+    W = "W"
+    NW = "NW"
+    SW = "SW"
+    NE = "NE"
+    SE = "SE"
+
+class Weather(Base):
+    __tablename__ = 'weather'
+    
+    id = Column(Integer, primary_key=True)
+    
+    country = Column(String)              
+    last_updated = Column(DateTime)      
+    wind_degree = Column(Integer)        
+    wind_kph = Column(Float)             
+    wind_direction = Column(Enum(WindDirection))
+    
+    celestial_id = Column(Integer, ForeignKey('celestial_events.id'))
+    celestial = relationship("CelestialEvents")
+
+class CelestialEvents(Base):
+    __tablename__ = 'celestial_events'
+    
+    id = Column(Integer, primary_key=True)
+    
+    sunrise = Column(Time)               
+    sunset = Column(Time)
+    moonrise = Column(Time)
+    moonset = Column(Time)
+    moon_phase = Column(String)
+    moon_illumination = Column(Integer)
+    
+    # Нова логічна колонка
+    should_go_outside = Column(Boolean, default=True)
